8fd4973fb7a6cc7c47dd06879dc35f531da4dbc5
ClementObspher <144014423+ClementObspher@users.noreply.github.com>
2024-03-19 14:11:12 +0100

    ignore main.tf

--- vpc.tf
+++ vpc.tf
@@ -2,9 +2,9 @@ resource "aws_vpc" "vpc_tooling" {
   cidr_block           = "10.8.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-    tags = {
-        Name = "local.vpc_name"
-    }
+  tags = {
+    Name = "local.vpc_name"
+  }
 }
 
 # resource "aws_internet_gateway" "igw_tooling" {
@@ -12,5 +12,5 @@ resource "aws_vpc" "vpc_tooling" {
 #     tags = {
 #         Name = "local.ig_name"
 #     }
-  
+
 # }
