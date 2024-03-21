6300287cc14fcf728d2a388ff1ec07f3decfaeaa
Maceo <basse.maceo@gmail.com>
2024-03-21 14:41:54 +0100

    Supprimer les règles de sécurité inutiles pour le groupe de sécurité nginx

--- ec2_nginx.tf
+++ ec2_nginx.tf
@@ -1,22 +1,22 @@
-resource "aws_security_group" "nginx_sg" {
-  name        = "nginx-sg"
-  description = "Security group for nginx instance allowing HTTP traffic"
-  vpc_id      = aws_vpc.my_vpc.id
+# resource "aws_security_group" "nginx_sg" {
+#   name        = "nginx-sg"
+#   description = "Security group for nginx instance allowing HTTP traffic"
+#   vpc_id      = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 # # Création de l'instance EC2 Ubuntu avec Nginx
 # resource "aws_instance" "nginx_instance" {
